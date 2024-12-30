0c141a601d12f820df69a640411d512fc6031f26
linrz <1210081098@qq.com>
2024-12-30 14:14:44 +0800

    lexical analysis complete

--- token/token.go
+++ token/token.go
@@ -21,6 +21,15 @@ const(
 	// 运算符
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// 分隔符
 	COMMA = ","
@@ -34,11 +43,21 @@ const(
 	// 关键字
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType{
