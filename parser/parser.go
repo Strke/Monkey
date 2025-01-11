497fbd01f921a34d2a0ec4785d781c2697e17f95
Linrz <1210081098@qq.com>
2025-01-12 00:19:44 +0800

    finish prefix

--- parser/parser.go
+++ parser/parser.go
@@ -138,6 +138,8 @@ func New(l *lexer.Lexer) *Parser{
 	p.prefixParserFns = make(map[token.TokenType]prefixParserFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	// 读取两个词法单元来设置当前token和下一个token
 	p.nextToken()
 	p.nextToken()
@@ -150,9 +152,17 @@ func (p *Parser) parseIdentifier() ast.Expression{
 	}
 }
 
-func (p *Parser)parseExpression(precedence int) ast.Expression{
+// 前缀为空的情况下报的错误
+func (p *Parser) noPrefixParseFnError(t token.TokenType){
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+
+func (p *Parser) parseExpression(precedence int) ast.Expression{
 	prefix := p.prefixParserFns[p.curToken.Type]
 	if prefix == nil{
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -169,4 +179,16 @@ func (p *Parser) parseIntegerLiteral() ast.Expression{
 	}
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression{
+	expression := &ast.PrefixExpression{
+		Token: p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
